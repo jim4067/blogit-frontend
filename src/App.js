e5ab253631425f369fe198e12fe46a21b93609a2
jim4067 <jimmyimpulse2@gmail.com>
2020-08-15 22:55:28 +0300

    added the variables and their states

--- src/App.js
+++ src/App.js
@@ -3,22 +3,28 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+	const [blogs, setBlogs] = useState([]);
+	const [newBlog, setNewBlog] = useState("");
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [user, setuser] = useState("");
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+	useEffect(() => {
+		blogService
+			.getAll()
+			.then(blogs =>
+				setBlogs(blogs)
+			)
+	}, []);
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
+	return (
+		<div>
+			<h2>blogs</h2>
+			{blogs.map(blog =>
+				<Blog key={blog.id} blog={blog} />
+			)}
+		</div>
+	)
 }
 
 export default App
