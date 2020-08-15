dc2cd5d600e4e3011849c20cfca714f0851f50db
jim4067 <jimmyimpulse2@gmail.com>
2020-08-15 23:11:47 +0300

    added the input form

--- src/App.js
+++ src/App.js
@@ -22,17 +22,23 @@ const App = () => {
 		console.log("logging in with", username, password);
 
 		try {
-			
+
 		} catch (exception) {
-			
+
 		}
 	}
 
 	return (
 		<div>
-			
-			<form>
-				
+
+			<form onSubmit={handleLogin}>
+				<div>
+					username <input type='text' name='username' value={username} onChange={({ target }) => setUsername(target.value)} />
+				</div>
+				<div>
+					password <input type='password' name='password' value={password} onChange={({ target }) => setPassword(target.value)} />
+				</div>
+				<button type='submit'>login</button>
 			</form>
 
 			<h2>blogs</h2>
