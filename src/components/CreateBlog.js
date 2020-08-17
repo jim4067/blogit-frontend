1d68ca0b1bcaa1a4a2a9550761b54bc6d4af39e3
jim4067 <jimmyimpulse2@gmail.com>
2020-08-17 17:40:04 +0300

    created a component to create new blogs

---
+++ src/components/CreateBlog.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//form component to create new blogs
+const CreateBlog = () => {
+
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default CreateBlog;
