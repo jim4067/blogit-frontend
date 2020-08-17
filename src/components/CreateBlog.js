7cdc7af59c51dbbff43152e088ab009edf138a20
jim4067 <jimmyimpulse2@gmail.com>
2020-08-17 18:17:45 +0300

    making the form for blog creation

--- src/components/CreateBlog.js
+++ src/components/CreateBlog.js
@@ -6,9 +6,25 @@ const CreateBlog = () => {
     const [newAuthor, setNewAuthor] = useState("");
     const [newUrl, setNewUrl] = useState("");
 
-    return(
+    //const [newBlog, setNewBlog] = useState("");
+
+    return (
         <div>
             <h2>CreateNew</h2>
+
+            <form onSubmit={CreateBlog} >
+                <div>
+                    title
+                </div>
+                <div>
+                    author
+                </div>
+                <div>
+                    url
+               </div>
+                <button type='submit'>create</button>
+            </form>
+
         </div>
     );
 }
