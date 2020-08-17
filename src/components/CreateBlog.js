587c6ca3c1df9944d2eef4cd433a29b3445e1f88
jim4067 <jimmyimpulse2@gmail.com>
2020-08-17 17:57:17 +0300

    added the state variables

--- src/components/CreateBlog.js
+++ src/components/CreateBlog.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //form component to create new blogs
 const CreateBlog = () => {
+    const [newTitle, setNewTitle] = useState("");
+    const [newAuthor, setNewAuthor] = useState("");
+    const [newUrl, setNewUrl] = useState("");
 
     return(
         <div>
