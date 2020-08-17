296a5dacf37c69e485ddf00bbdc3350bd8f541bd
jim4067 <jimmyimpulse2@gmail.com>
2020-08-17 14:13:12 +0300

    added the login event hanlder, and adding new blogs, and the user's state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import React, { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+import loginService from './services/login';
+import './App.css';
 
 const App = () => {
 	const [blogs, setBlogs] = useState([]);
 	const [newBlog, setNewBlog] = useState("");
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
-	const [user, setuser] = useState("");
+	const [user, setUser] = useState("");
 
 	useEffect(() => {
 		blogService
@@ -22,9 +24,15 @@ const App = () => {
 		console.log("logging in with", username, password);
 
 		try {
+			const user = await loginService.login(
+				{username, password}
+			);
 
+			setUsername("");
+			setPassword("");
+			setUser(user);
 		} catch (exception) {
-
+			console.log("the exception ", exception);
 		}
 	}
 
