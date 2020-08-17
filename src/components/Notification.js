110bfc7ed7b016d175201432823ec31939369963
jim4067 <jimmyimpulse2@gmail.com>
2020-08-17 20:32:28 +0300

    added the styling for the wrong login

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
+const wrong_login_styles={
+    "border" : "3px solid red",
+    "border-radius" : "3px" ,
+    "color" : "red",
+    "font-size" : "20px",
+    "margin-top" : "10px",
+    "padding" : "10px",
+    "text-align" : "center"
+}
+
 const Notification = ({message}) => {
 
     if(message === null) {
         return null
     } else {
         return(
-            <div className='error'>
+            <div className='error' style={wrong_login_styles}>
                 {message}
             </div>
         )
