fd94dfd497ffdaf8ba8da9b05d8b23e922ece2c6
jim4067 <jimmyimpulse2@gmail.com>
2020-08-17 16:35:57 +0300

    the get now uses aync syntax

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -7,10 +7,11 @@ const setToken = (newToken) => {
   token = `bearer ${newToken}`
 }
 
-const getAll = () => {
-  const request = axios.get(baseUrl);
-
-  return request.then(response => response.data)
+const getAll = async () => {
+  const response = await axios.get(baseUrl);
+  
+  console.log(response.data);
+  return response.data;
 }
 
 const create = async (blogObject) => {
