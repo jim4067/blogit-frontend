b4e10341e7cd2f7d97c1e377b7284968032fa232
jim4067 <jimmyimpulse2@gmail.com>
2020-08-17 16:20:42 +0300

    removed the login form into their own components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect, Component } from 'react';
-import Blog from './components/Blog';
+import React, { useState, useEffect } from 'react';
+import DisplayBlogs from './components/DisplayBlogs';
+import LoginForm from './components/LoginForm';
 import blogService from './services/blogs';
 import loginService from './services/login';
 import './App.css';
@@ -36,46 +37,18 @@ const App = () => {
 		}
 	}
 
-	//component to display login
-	const LoginForm = () => {
-
-		return (
-			<form onSubmit={handleLogin}>
-				<div>
-					username <input type='text' name='username' value={username} onChange={({ target }) => setUsername(target.value)} />
-				</div>
-				<div>
-					password <input type='password' name='password' value={password} onChange={({ target }) => setPassword(target.value)} />
-				</div>
-				<button type='submit'>login</button>
-			</form>
-		);
-	}
-
-	//component to display blogs if user logges in
-	const DisplayBlogs = () => {
-
-		return(
-			<div>
-				<h2>Blogs</h2>
-				{blogs.map((blog) =>
-				<Blog key={blog.id} blog={blog} />
-			)}
-			</div>
-		);
-	}
-
 	return (
 		<div>
 			{user === ""
-			?
-			<LoginForm />
-			:
-			<DisplayBlogs />
+				?
+				<LoginForm handleLogin={handleLogin} username={username} setUsername={setUsername}
+					password={password} setPassword={setPassword} />
+				:
+				<DisplayBlogs blogs={blogs} />
 			}
 
 		</div>
 	);
 }
 
-export default App
+export default App;
