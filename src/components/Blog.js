1386a1b4776e65806f3580699fd2518620e8bc95
jim4067 <jimmyimpulse2@gmail.com>
2020-08-20 00:38:49 +0300

    added the main styles to distinguish the blogs

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,16 +1,29 @@
 import React from 'react'
 
+const mainStyles = {
+  "border": "solid",
+  "margin" : "3px",
+  "padding" : "4px"
+}
+
+const HideStyles = {
+
+}
+
 const Blog = ({ blog }) => (
 
-  <div>
+  <div style={mainStyles} >
     {blog.title} {blog.author}<button>view</button>
-    <div>
+    <div >
       {blog.url} <br />
-      {blog.likes} <br />
+      likes: {blog.likes} <br />
       {blog.author}
     </div>
-    
+
   </div>
 )
 
-export default Blog
+export default Blog;
+
+//the idea is to have the button hide the info in the second div when a button is pressed
+//the state will alway be set to true ie show
