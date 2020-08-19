5a177ee47178fa97b6582547f9b7a9d8a02f1049
jim4067 <jimmyimpulse2@gmail.com>
2020-08-19 15:59:13 +0300

    more comments to make life easier

--- src/components/Togglable.js
+++ src/components/Togglable.js
@@ -4,8 +4,10 @@ const Togglable = (props) => {
     const [visible, setVisible] = useState(false)
 
     //by default the login button is set to display:"" since state is false
+    //in short -> show button and hide the login form
     const hideWhenVisible = { display: visible ? "none" : "" };
     //by default the form display is set to display:"none" since state has changed to true
+    //in short -> hide button for login and display hte noteform with the cancel button
     const showWhenVisible = { display: visible ? "" : "none" };
 
     const toggleVisibility = () => {
