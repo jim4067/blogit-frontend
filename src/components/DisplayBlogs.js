218719897c04ec1d49f06bb3f30f03b6bdde0241
jim4067 <jimmyimpulse2@gmail.com>
2020-08-19 20:49:02 +0300

    removed the Blog form from  here

--- src/components/DisplayBlogs.js
+++ src/components/DisplayBlogs.js
@@ -3,22 +3,18 @@ import Blog from './Blog';
 import BlogForm from './BlogForm';
 
 //component to display blogs if user logges in
-const DisplayBlogs = ({ blogs, user, handleLogout, createBlog }) => {
+const DisplayBlogs = (props) => {
 
     return (
         <div>
             <h2>Blogs</h2>
 
             <p>
-                {`logged in as ${user.name}`}
-                <button onClick={handleLogout}>logout</button>
+                {`logged in as ${props.user.name}`}
+                <button onClick={props.handleLogout}>logout</button>
             </p>
 
-            <BlogForm createBlog={createBlog} />
-
-            {blogs.map((blog) =>
-                <Blog key={blog.id} blog={blog} />
-            )}
+            {props.children}
 
         </div>
     );
@@ -26,3 +22,12 @@ const DisplayBlogs = ({ blogs, user, handleLogout, createBlog }) => {
 
 
 export default DisplayBlogs;
+
+/*
+<BlogForm createBlog={createBlog} />
+
+            {blogs.map((blog) =>
+                <Blog key={blog.id} blog={blog} />
+            )}
+
+*/
