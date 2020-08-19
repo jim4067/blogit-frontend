c6d887c6db75b998bea70b41764a940cdf3a04d0
jim4067 <jimmyimpulse2@gmail.com>
2020-08-19 15:56:09 +0300

    added commentary to the togglable component

--- src/components/Togglable.js
+++ src/components/Togglable.js
@@ -3,7 +3,9 @@ import React, { useState } from 'react';
 const Togglable = (props) => {
     const [visible, setVisible] = useState(false)
 
+    //by default the login button is set to display:"" since state is false
     const hideWhenVisible = { display: visible ? "none" : "" };
+    //by default the form display is set to display:none since state has changed to true
     const showWhenVisible = { display: visible ? "" : "none" };
 
     const toggleVisibility = () => {
