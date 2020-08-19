eb1797cd7c4b90dcb030a8e7685b902dc53abdb8
jim4067 <jimmyimpulse2@gmail.com>
2020-08-20 00:32:12 +0300

    now displaying all the info about the blogs

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
 const Blog = ({ blog }) => (
+
   <div>
     {blog.title} {blog.author}<button>view</button>
+    <div>
+      {blog.url} <br />
+      {blog.likes} <br />
+      {blog.author}
+    </div>
+    
   </div>
 )
 
