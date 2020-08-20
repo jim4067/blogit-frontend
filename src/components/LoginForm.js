476810a8f7a27377e912342c67fc86488000e3e0
jim4067 <jimmyimpulse2@gmail.com>
2020-08-20 22:55:46 +0300

    formatted the LoginForm component according to the eslint rules

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,27 +2,27 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 //component to display login
-	const LoginForm = ({handleLogin, username, setUsername, password, setPassword}) => {
+const LoginForm = ({ handleLogin, username, setUsername, password, setPassword }) => {
 
-		return (
-			<form onSubmit={handleLogin}>
-				<div>
-					username <input type='text' name='username' value={username} onChange={({ target }) => setUsername(target.value)} />
-				</div>
-				<div>
-					password <input type='password' name='password' value={password} onChange={({ target }) => setPassword(target.value)} />
-				</div>
-				<button type='submit'>login</button>
-			</form>
-		);
-	}
+	return (
+		<form onSubmit={handleLogin}>
+			<div>
+				username <input type='text' name='username' value={username} onChange={({ target }) => setUsername(target.value)} />
+			</div>
+			<div>
+				password <input type='password' name='password' value={password} onChange={({ target }) => setPassword(target.value)} />
+			</div>
+			<button type='submit'>login</button>
+		</form>
+	);
+}
 
-	LoginForm.propTypes = {
-		handleLogin : propTypes.func.isRequired,
-		username : propTypes.string.isRequired,
-		setUsername : propTypes.func.isRequired,
-		password : propTypes.string.isRequired,
-		setPassword : propTypes.func.isRequired
-	}
+LoginForm.propTypes = {
+	handleLogin: propTypes.func.isRequired,
+	username: propTypes.string.isRequired,
+	setUsername: propTypes.func.isRequired,
+	password: propTypes.string.isRequired,
+	setPassword: propTypes.func.isRequired
+}
 
-    export default LoginForm;
+export default LoginForm;
