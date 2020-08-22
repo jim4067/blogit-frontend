5f64a6f51f71f00ca1dd527645f4c4398a8a1dfc
jim4067 <jimmyimpulse2@gmail.com>
2020-08-22 20:46:33 +0300

    :rocket: :zap: :tada:

---
+++ src/components/BlogForm.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, fireEvent } from '@testing-library/react';
+import BlogForm from './BlogForm';
+
+test("test whether the component is called with the correct props", () => {
+    const createBlog = jest.fn();
+});
