dfb236a100a19bb20a5b6f0c5798c450b123bcb7
jim4067 <jimmyimpulse2@gmail.com>
2020-08-24 13:23:12 +0300

    modified the test description

--- src/components/BlogForm.test.js
+++ src/components/BlogForm.test.js
@@ -3,7 +3,7 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, fireEvent } from '@testing-library/react';
 import BlogForm from './BlogForm';
 
-test("test whether the component is called with the correct props", () => {
+test("test whether the component called with the correct props is updated", () => {
     const createBlog = jest.fn();
 
     const component = render(
