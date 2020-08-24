c58a7a376f03e041ef53946251f9f282b23cf847
jim4067 <jimmyimpulse2@gmail.com>
2020-08-24 13:14:25 +0300

    finished 5.16*: Blog list tests, step4

--- src/components/BlogForm.test.js
+++ src/components/BlogForm.test.js
@@ -5,4 +5,20 @@ import BlogForm from './BlogForm';
 
 test("test whether the component is called with the correct props", () => {
     const createBlog = jest.fn();
+
+    const component = render(
+        <BlogForm createBlog={createBlog} />
+    );
+    component.debug();
+
+    const author = component.container.querySelector('#author');
+    const form = component.container.querySelector('form');
+
+    fireEvent.change(author, {
+        target: { value : "Jimmy Impulse" }
+    });
+    fireEvent.submit(form);
+
+    expect(createBlog.mock.calls).toHaveLength(1);
+    expect(createBlog.mock.calls[0][0].author).toBe("Jimmy Impulse");
 });
