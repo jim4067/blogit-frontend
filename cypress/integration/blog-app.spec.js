ef38c68fb938f8bbe0344cb71d746dc5b16de2ee
jim4067 <jimmyimpulse2@gmail.com>
2020-08-31 22:59:35 +0300

    finished 5.18: bloglist end to end testing, step2

--- cypress/integration/blog-app.spec.js
+++ cypress/integration/blog-app.spec.js
@@ -1,6 +1,6 @@
-describe("blog app" , function (){
+describe("blog app", function () {
     //this is where it began. The errors, the terros and everything in between
-    beforeEach(function() {
+    beforeEach(function () {
         //cy.request('POST', 'http://localhost:3030/api/testing/reset');
         cy.visit('http://localhost:3000');
     });
@@ -8,4 +8,28 @@ describe("blog app" , function (){
     it("Login form is showed", function () {
         cy.contains("login").click();
     });
-} );
+
+    describe("logging in", function () {
+        it("succeeds with the correct credentials", function () {
+            cy.contains("login").click();
+            cy.get('#username').type("jim4067");
+            cy.get('#password').type("pass123");
+            cy.get('#login-button').click()
+
+            cy.contains("logged in as");
+        });
+
+        it("fails with wrong credentials", function () {
+            cy.contains("login").click();
+            cy.get('#username').type("wrong_user");
+            cy.get('#password').type("wrong_pass");
+            cy.get('#login-button').click();
+
+            cy.contains("wrong credentials")
+        });
+    });
+
+
+
+
+});
