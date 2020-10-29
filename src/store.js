4442b443dae8bd8824d83272509d45f09001e5ef
jim4067 <jimmyimpulse2@gmail.com>
2020-10-29 16:04:56 +0300

    feat: created a store for redux

---
+++ src/store.js
@@ -0,0 +1,16 @@
+import { createStore, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import notificationReducer from './src/reducers/notificationReducer';
+
+
+const reducer = combineReducers({
+    notification: notificationReducer,
+});
+
+const store = createStore(reducer, composeWithDevTools());
+
+export default store;
+
+
+//the store for the blog listing apps state. Should it have been in the src directory;
