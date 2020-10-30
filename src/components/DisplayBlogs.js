e4da5b45f4d248392f712bfa24980722fa1cb2ba
jim4067 <jimmyimpulse2@gmail.com>
2020-10-30 20:26:49 +0300

    feat: changing the component so as to be able to use the redux store

--- src/components/DisplayBlogs.js
+++ src/components/DisplayBlogs.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Blog from './Blog';
 
 //component to display blogs if user logges in
 const DisplayBlogs = (props) => {
+    const blogs = useSelector(state => state.blogs);
 
     return (
         <div>
             <h2>Blogs</h2>
 
-            <p>
+            {/* <p>
                 {`logged in as ${props.user.name}`}
                 <button onClick={props.handleLogout}>logout</button>
-            </p>
+            </p> */}
 
-            {props.children}
+            {
+                blogs.map((blog) =>
+                    <Blog key={blog.id} blog={blog} />
+            )}
+
+            {/* {props.children} */}
 
         </div>
     );
