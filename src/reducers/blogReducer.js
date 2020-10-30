d6b26d4d4dec7b40dbf14a018588d2c66ec1e6e8
jim4067 <jimmyimpulse2@gmail.com>
2020-10-30 23:53:52 +0300

    feat: finished the reducer for the blogs and actions creators for initializing, creating, liking and removing blogs

---
+++ src/reducers/blogReducer.js
@@ -0,0 +1,80 @@
+import blogService from '../services/blogs';
+
+//function to sort the blogs
+const sortBlogs = (blogs) => {
+    return blogs.sort((a, b) => b.likes - a.likes);
+}
+
+const blogReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'INIT BLOGS':
+            return sortBlogs(action.data);
+        case 'NEW BLOG':
+            return [...state, action.data];
+        case 'VOTE': {
+            const likedBlog = action.updatedBlog;
+            const blogsToSort = state.map(blog => blog.id === likedBlog.id ? likedBlog : blog);
+            return sortBlogs(blogsToSort);
+        }
+        case 'REMOVE':
+            return sortBlogs(action.data);
+        default:
+            return state;
+    }
+}
+
+
+export const initializeState = () => {
+    return async dispatch => {
+        const blogs = await blogService.getAll();
+
+        dispatch({
+            type: 'INIT BLOGS',
+            data: blogs
+        });
+    }
+}
+
+export const newBlog = (content) => {
+    return async dispatch => {
+        const addedBlog = await blogService.create(content);
+
+        dispatch({
+            type: 'NEW BLOG',
+            data: addedBlog
+        })
+    }
+}
+
+export const likeBlog = (id) => {
+    return async dispatch => {
+        const allBlogs = await blogService.getAll();
+        const blogToLike = allBlogs.find(blog => blog.id === id);
+        const likedBlog = { ...blogToLike, likes: blogToLike.likes + 1 };
+        const updatedBlog = await blogService.update(id, likedBlog);
+        console.log(updatedBlog.id);
+
+        dispatch({
+            type: 'VOTE',
+            updatedBlog
+        })
+    }
+}
+
+export const removeBlog = (id) => {
+    return async dispatch => {
+        await blogService.remove(id);
+        const allBlogs = await blogService.getAll();
+        const afterDeletion = allBlogs.filter(blogs => blogs.id !== id);
+
+        dispatch({
+            type: 'REMOVE',
+            data: afterDeletion
+        })
+    }
+}
+
+export default blogReducer;
+
+//make functions in the app component so as to make sure that the notifications
+//are dispatched too
