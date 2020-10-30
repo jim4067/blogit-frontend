a52a14ccb840bb4482d029bd4fa01f898915a821
jim4067 <jimmyimpulse2@gmail.com>
2020-10-30 14:58:54 +0300

    feat: using redux-thunk for the async notification reducer

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
 import notificationReducer from './reducers/notificationReducer';
 
@@ -7,7 +8,11 @@ const reducer = combineReducers({
     notification: notificationReducer,
 });
 
-const store = createStore(reducer, composeWithDevTools());
+const store = createStore(
+    reducer, composeWithDevTools(
+        applyMiddleware(thunk)
+    )
+);
 
 export default store;
 
