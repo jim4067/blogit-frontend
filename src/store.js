e29c3d924027a05d1a0149a410c5ddab028549bd
jim4067 <jimmyimpulse2@gmail.com>
2020-10-30 20:10:26 +0300

    added the blogs to the root reducer

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import blogReducer from './reducers/blogReducer';
 
 import notificationReducer from './reducers/notificationReducer';
 
 const reducer = combineReducers({
     notification: notificationReducer,
+    blogs: blogReducer,
 });
 
 const store = createStore(
