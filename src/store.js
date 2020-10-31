bcc50cc90602914aaefb412e6faec46661780e1d
jim4067 <jimmyimpulse2@gmail.com>
2020-10-31 09:46:23 +0300

    feat: imported the userReducer into the root reducer

--- src/store.js
+++ src/store.js
@@ -1,12 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import blogReducer from './reducers/blogReducer';
 
+import blogReducer from './reducers/blogReducer';
 import notificationReducer from './reducers/notificationReducer';
+import userReducer from './reducers/userReducer';
 
 const reducer = combineReducers({
     notification: notificationReducer,
+    user: userReducer,
     blogs: blogReducer,
 });
 
