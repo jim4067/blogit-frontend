db908f0fde066aa07d79a44bd1128238ac9bd8b7
jim4067 <jimmyimpulse2@gmail.com>
2020-10-31 09:48:09 +0300

    feat: created the reducer for users

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,25 @@
+//import loginService from '../services/login';
+
+//Store the information about the signed in user in the Redux store.
+
+const userReducer = (state = "", action) => {
+    switch (action.type) {
+        case 'INIT USER':
+            return action.data;
+        default:
+            return state;
+    }
+}
+
+//the action creator for storing the user go in here
+//guess that the user will have to log in here and the data returned will be user
+// Should I move the entire login function here?
+
+export const loggedUser = (user) => {
+    return {
+        type: 'INIT USER',
+        data: user
+    }
+}
+
+export default userReducer;
