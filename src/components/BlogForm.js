4917029bfa41d16301331d6084a9ec48f2e49691
jim4067 <jimmyimpulse2@gmail.com>
2020-11-01 19:26:31 +0300

    feat: using state and action creators from blogReducer for creation of new

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -1,26 +1,36 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import { newBlog } from '../reducers/blogReducer';
+import { showNotification } from '../reducers/notificationReducer';
 
 //form component to create new blogs
-const BlogForm = ({ createBlog }) => {
-
-    //the states for value when creating a new blog
-    const [newTitle, setNewTitle] = useState("");
-    const [newAuthor, setNewAuthor] = useState("");
-    const [newUrl, setNewUrl] = useState("");
+const BlogForm = () => {
+    const dispatch = useDispatch();
 
     const addBlog = (event) => {
 
         event.preventDefault();
 
-        createBlog({
-            title: newTitle,
-            author: newAuthor,
-            url: newUrl
-        });
+        //the data from the form
+        const title = event.target.title.value;
+        event.target.title.value = "";
+        const author = event.target.author.value;
+        event.target.author.value = "";
+        const url = event.target.url.value;
+        event.target.url.value = "";
 
-        setNewTitle("");
-        setNewAuthor("");
-        setNewUrl("");
+        const blogObject = {
+            title,
+            author,
+            url
+        }
+
+        //action creator for a new blog
+        dispatch(newBlog(blogObject));
+
+        //action creator for showing notifications
+        dispatch(showNotification(`a new blog -> ${blogObject.title} by ${blogObject.author} added`, 5));
     }
 
     return (
@@ -29,13 +39,16 @@ const BlogForm = ({ createBlog }) => {
 
             <form onSubmit={addBlog} >
                 <div>
-                    title <input type='text' name='title' value={newTitle} onChange={({ target }) => setNewTitle(target.value)} />
+                    {/* title <input type='text' name='title' value={newTitle} onChange={({ target }) => setNewTitle(target.value)} /> */}
+                    title <input type='text' name='title' />
                 </div>
                 <div>
-                    author <input id='author' type='text' name='author' value={newAuthor} onChange={({ target }) => setNewAuthor(target.value)} />
+                    {/* author <input id='author' type='text' name='author' value={newAuthor} onChange={({ target }) => setNewAuthor(target.value)} /> */}
+                    author <input type='text' name='author' />
                 </div>
                 <div>
-                    url <input type='text' name='url' value={newUrl} onChange={({ target }) => setNewUrl(target.value)} />
+                    {/* url <input type='text' name='url' value={newUrl} onChange={({ target }) => setNewUrl(target.value)} /> */}
+                    url <input type='text' name='url' />
                 </div>
                 <button type='submit'>create</button>
             </form>
