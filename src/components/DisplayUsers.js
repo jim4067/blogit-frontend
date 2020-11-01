3e6489a2e11d206b1d62d22a2574a184e8920794
jim4067 <jimmyimpulse2@gmail.com>
2020-11-01 17:49:25 +0300

    feat: added reducer for managing users

---
+++ src/components/DisplayUsers.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+//this is the component that is going to list all the users
+//create a services module that makes a request to the api endpoint /api/users in the user reducer 
+//and import it here. From there on map the data received here
+//return user.name for the names of the user
