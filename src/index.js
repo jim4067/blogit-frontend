2e2b7b57d8cb883da22e65c6a9a28fc2a465e8ce
jim4067 <jimmyimpulse2@gmail.com>
2020-11-02 16:10:00 +0300

    feat: imported BrowserRouter from react-router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import store from './store';
 import './styles/index.css';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <Router>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </Router>,
     document.getElementById('root')
 )
