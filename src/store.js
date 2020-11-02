6a8f43d369803b429b48b2a25165aac9dae51dd6
jim4067 <jimmyimpulse2@gmail.com>
2020-11-02 14:03:03 +0300

    feat: added the reducer for users to the store

--- src/store.js
+++ src/store.js
@@ -5,11 +5,13 @@ import thunk from 'redux-thunk';
 import blogReducer from './reducers/blogReducer';
 import notificationReducer from './reducers/notificationReducer';
 import userReducer from './reducers/userReducer';
+import usersReducer from './reducers/usersReducer';
 
 const reducer = combineReducers({
+    blogs: blogReducer,
     notification: notificationReducer,
     user: userReducer,
-    blogs: blogReducer,
+    users: usersReducer
 });
 
 const store = createStore(
